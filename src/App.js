1b7864ee9c011932511866466c1fd403ddfe8e09
paperfox <ndg.paperfox@gmail.com>
2024-07-12 18:21:45 -0400

    Website
    more adjustments to landing page

--- src/App.js
+++ src/App.js
@@ -6,23 +6,78 @@ import Footer from './Footer';
 function App() {
   return (
     <main>
-      <div className="row">
+      <div className="container">
+        <div className="row pt-5">
+          <div className="col-md-5 pt-5"><hr/></div>
+          <div className="col-md-2">
+            <img src="https://paperfox.github.io/images/paperfoxlogo.svg" alt="placeholder" className="m-5 mt-0"/>
+          </div>
+          <div className="col-md-5 pt-5"><hr/></div>
+        </div>
+        <h1></h1>
+        <h2>Art</h2>
+        <p>You can find my art up on Instagram or Cara.</p>
+        <div className="row">
+          <div className="col-sm">
+            <a href="">
+            <div className="mx-4 build-deck">
+                <img src="https://paperfox.github.io/images/cards/back.jpg" className="deck-card-stack" alt="" />
+                <img src="https://paperfox.github.io/images/cards/back.jpg" className="deck-card-stack" alt="" />
+                <img src="https://paperfox.github.io/images/cards/back.jpg" className="deck-card-stack" alt="" />
+                <img src="https://paperfox.github.io/images/cards/back.jpg" className="deck-card-stack" alt="" />
+                <img src="https://paperfox.github.io/images/cards/back.jpg" className="deck-card-stack" alt="" />
+                <img src="https://paperfox.github.io/images/cards/back.jpg" className="deck-card-stack " alt="" id="top" />
+              </div>
+            </a>
+          </div>
+          <div className="col-sm">
+            <a href="">
+              <img src="https://paperfox.github.io/images/cards/back.jpg"  className="w-100" />
+            </a>
+          </div>
+          <div className="col-sm">
+            <a href="">
+              <img src="https://paperfox.github.io/images/cards/back.jpg"  className="w-100" />
+            </a>
+          </div>
+
+        </div>
+        <h2>Tarot</h2>
+        <h2>Where to Find Me</h2>
+        <p>Looking to connect? You can find me in any of these places.</p>
+
+        <ul className="list-unstyled d-flex justify-content-center">
+          <li className="m-3">
+              <a href="https://www.instagram.com/paperfoxmakesart/" target="_blank" rel="noreferrer"><img src="../images/instagramlogo.svg" alt="instagram" /></a>
+          </li>
+          <li className="m-3">
+              <a href="https://cara.app/paperfox" target="_blank" rel="noreferrer"><img src="../images/cara-app-logo-circle-White.svg" alt="cara" /></a>
+          </li>
+          <li className="m-3">
+              <a href="https://github.com/paperfox" target="_blank" rel="noreferrer"><img src="../images/github-mark.svg" alt="github"/></a>
+          </li>
+          <li className="m-3">
+              <a href="https://www.linkedin.com/in/paperfox/" target="_blank" rel="noreferrer"><img src="../images/linkedinlogo.svg" alt="linked in" /></a>
+          </li>
+        </ul>
+      </div>
+      {/* <div className="row">
         <div className="col-md-3">
-          {/* <img src="../images/paperfoxlogo.svg" alt="placeholder" /> */}
+          <img src="../images/paperfoxlogo.svg" alt="placeholder" />
           <img src="https://paperfox.github.io/images/paperfoxlogo.svg" alt="placeholder" />
 
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="https://paperfox.github.io">Home</a>
+          <ul className="nav flex-column">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="https://paperfox.github.io">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://paperfox.github.io">Artwork</a>
+            <li className="nav-item">
+              <a className="nav-link" href="https://paperfox.github.io">Artwork</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://paperfox.github.io/tarot">Tarot</a>
+            <li className="nav-item">
+              <a className="nav-link" href="https://paperfox.github.io/tarot">Tarot</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="https://paperfox.github.io">About & Contact</a>
+            <li className="nav-item">
+              <a className="nav-link" href="https://paperfox.github.io">About & Contact</a>
             </li>
           </ul>
         </div>
@@ -41,7 +96,7 @@ function App() {
             </Tab>
           </Tabs>
         </div>
-      </div>
+      </div> */}
 
       <Footer />
     </main>
