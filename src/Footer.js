5c93bb08e4ce920cd46c4d367d66e4624e00a793
paperfox <ndg.paperfox@gmail.com>
2024-07-13 14:33:42 -0400

    Updates

--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="m-4 pb-2">
-        <p className="text-center"><small>© {new Date().getFullYear()} Nathalie Garfinkle</small></p>
-    </footer>
-  );
-}
-
-export default Footer;
