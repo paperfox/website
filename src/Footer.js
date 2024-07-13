16fdbf0caaebe9b41eb1a1fa5638272919272cec
paperfox <ndg.paperfox@gmail.com>
2024-07-13 13:07:42 -0400

    Homepage
    more work on getting pages reorged

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="m-4 pb-2">
+        <p className="text-center"><small>© {new Date().getFullYear()} Nathalie Garfinkle</small></p>
+    </footer>
+  );
+}
+
+export default Footer;
